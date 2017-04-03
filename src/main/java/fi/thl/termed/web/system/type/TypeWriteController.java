29ecdf0fab22aea3ad35b9c3808670d760fbbf03
Jussi Kurki <jussi.kurki@iki.fi>
2017-04-03 16:06:18 +0300

    method for replacing all graph types in one call

--- src/main/java/fi/thl/termed/web/system/type/TypeWriteController.java
+++ src/main/java/fi/thl/termed/web/system/type/TypeWriteController.java
@@ -1,17 +1,6 @@
 package fi.thl.termed.web.system.type;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.UUID;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 import fi.thl.termed.domain.Graph;
 import fi.thl.termed.domain.GraphId;
@@ -23,8 +12,18 @@ import fi.thl.termed.util.service.Service;
 import fi.thl.termed.util.spring.annotation.PostJsonMapping;
 import fi.thl.termed.util.spring.annotation.PutJsonMapping;
 import fi.thl.termed.util.spring.exception.NotFoundException;
-
-import static org.springframework.http.HttpStatus.NO_CONTENT;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/graphs/{graphId}/types")
@@ -54,6 +53,21 @@ public class TypeWriteController {
       @AuthenticationPrincipal User currentUser) {
     types.forEach(type -> type.setGraph(new GraphId(graphId)));
     typeService.save(types, currentUser);
+  }
+
+  @PostJsonMapping(params = {"batch=true", "replace=true"}, produces = {})
+  @ResponseStatus(NO_CONTENT)
+  public void replace(
+      @PathVariable("graphId") UUID graphId,
+      @RequestBody List<Type> types,
+      @AuthenticationPrincipal User user) {
+    types.forEach(type -> type.setGraph(new GraphId(graphId)));
+
+    List<TypeId> removedTypes = typeService.getKeys(new TypesByGraphId(graphId), user);
+    removedTypes.removeAll(types.stream().map(TypeId::new).collect(Collectors.toList()));
+    typeService.delete(removedTypes, user);
+
+    typeService.save(types, user);
   }
 
   @PutJsonMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
