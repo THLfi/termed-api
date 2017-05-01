c4aeca64fa331ddd249d4fc39a0b7bf1b461528a
Jussi Kurki <jussi.kurki@iki.fi>
2017-05-01 12:23:34 +0300

    lazy loading node tree optimized. loop detection removed

--- src/main/java/fi/thl/termed/domain/LazyLoadingNodeTree.java
+++ src/main/java/fi/thl/termed/domain/LazyLoadingNodeTree.java
@@ -1,8 +1,10 @@
 package fi.thl.termed.domain;
 
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.ImmutableSet;
+import static com.google.common.collect.Multimaps.transformEntries;
+
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Table;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -12,33 +14,16 @@ public class LazyLoadingNodeTree implements NodeTree {
 
   private Node source;
 
-  private ImmutableMultimap<String, NodeTree> treeReferences;
-  private ImmutableMultimap<String, NodeTree> treeReferrers;
-
-  private ImmutableSet<NodeId> referencePath;
-  private ImmutableSet<NodeId> referrerPath;
-
   private BiFunction<Node, String, List<Node>> referenceProvider;
   private BiFunction<Node, String, List<Node>> referrerProvider;
+
+  private Table<String, NodeId, NodeTree> referenceCache = HashBasedTable.create();
+  private Table<String, NodeId, NodeTree> referrerCache = HashBasedTable.create();
 
   public LazyLoadingNodeTree(Node source,
       BiFunction<Node, String, List<Node>> referenceProvider,
       BiFunction<Node, String, List<Node>> referrerProvider) {
-    this(source,
-        ImmutableSet.of(source.identifier()),
-        ImmutableSet.of(source.identifier()),
-        referenceProvider,
-        referrerProvider);
-  }
-
-  private LazyLoadingNodeTree(Node source,
-      ImmutableSet<NodeId> referencePath,
-      ImmutableSet<NodeId> referrerPath,
-      BiFunction<Node, String, List<Node>> referenceProvider,
-      BiFunction<Node, String, List<Node>> referrerProvider) {
     this.source = source;
-    this.referencePath = referencePath;
-    this.referrerPath = referrerPath;
     this.referenceProvider = referenceProvider;
     this.referrerProvider = referrerProvider;
   }
@@ -90,66 +75,30 @@ public class LazyLoadingNodeTree implements NodeTree {
 
   @Override
   public Multimap<String, NodeTree> getReferences() {
-    if (treeReferences == null) {
-      treeReferences = loadReferences();
-    }
-    return treeReferences;
+    return transformEntries(source.getReferences(), this::loadReference);
   }
 
-  private ImmutableMultimap<String, NodeTree> loadReferences() {
-    ImmutableMultimap.Builder<String, NodeTree> rs = ImmutableMultimap.builder();
-
-    for (String attributeId : source.getReferences().keySet()) {
-      for (Node reference : referenceProvider.apply(source, attributeId)) {
-        if (!referencePath.contains(reference.identifier())) {
-          rs.put(attributeId, newLazyLoadingReferenceTree(reference));
-        }
-      }
+  private NodeTree loadReference(String attributeId, NodeId valueId) {
+    if (!referenceCache.contains(attributeId, valueId)) {
+      referenceProvider.apply(source, attributeId)
+          .forEach(value -> referenceCache.put(attributeId, value.identifier(),
+              new LazyLoadingNodeTree(value, referenceProvider, referrerProvider)));
     }
-
-    return rs.build();
-  }
-
-  private LazyLoadingNodeTree newLazyLoadingReferenceTree(Node reference) {
-    return new LazyLoadingNodeTree(reference,
-        copyAndAppend(referencePath, reference.identifier()),
-        referrerPath,
-        referenceProvider,
-        referrerProvider);
+    return referenceCache.get(attributeId, valueId);
   }
 
   @Override
   public Multimap<String, NodeTree> getReferrers() {
-    if (treeReferrers == null) {
-      treeReferrers = loadReferrers();
+    return transformEntries(source.getReferrers(), this::loadReferrer);
+  }
+
+  private NodeTree loadReferrer(String attributeId, NodeId valueId) {
+    if (!referrerCache.contains(attributeId, valueId)) {
+      referrerProvider.apply(source, attributeId)
+          .forEach(value -> referrerCache.put(attributeId, value.identifier(),
+              new LazyLoadingNodeTree(value, referenceProvider, referrerProvider)));
     }
-    return treeReferrers;
-  }
-
-  private ImmutableMultimap<String, NodeTree> loadReferrers() {
-    ImmutableMultimap.Builder<String, NodeTree> rs = ImmutableMultimap.builder();
-
-    for (String attributeId : source.getReferrers().keySet()) {
-      for (Node referrer : referrerProvider.apply(source, attributeId)) {
-        if (!referrerPath.contains(referrer.identifier())) {
-          rs.put(attributeId, newLazyLoadingReferrerTree(referrer));
-        }
-      }
-    }
-
-    return rs.build();
-  }
-
-  private LazyLoadingNodeTree newLazyLoadingReferrerTree(Node referrer) {
-    return new LazyLoadingNodeTree(referrer,
-        referencePath,
-        copyAndAppend(referrerPath, referrer.identifier()),
-        referenceProvider,
-        referrerProvider);
-  }
-
-  private <T> ImmutableSet<T> copyAndAppend(ImmutableSet<T> set, T last) {
-    return ImmutableSet.<T>builder().addAll(set).add(last).build();
+    return referrerCache.get(attributeId, valueId);
   }
 
 }
