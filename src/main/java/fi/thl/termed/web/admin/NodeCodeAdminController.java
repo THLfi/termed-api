7979650b920b346fdc2ea13059d2917e4a74f074
Jussi Kurki <jussi.kurki@iki.fi>
2018-05-29 14:58:19 +0300

    change admin api to delete node codes in graphs

--- src/main/java/fi/thl/termed/web/admin/NodeCodeAdminController.java
+++ src/main/java/fi/thl/termed/web/admin/NodeCodeAdminController.java
@@ -8,19 +8,13 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import fi.thl.termed.domain.AppRole;
 import fi.thl.termed.domain.Node;
 import fi.thl.termed.domain.NodeId;
-import fi.thl.termed.domain.Type;
-import fi.thl.termed.domain.TypeId;
 import fi.thl.termed.domain.User;
 import fi.thl.termed.service.node.specification.NodesByGraphId;
 import fi.thl.termed.service.node.specification.NodesByTypeId;
-import fi.thl.termed.service.type.specification.TypesByGraphId;
 import fi.thl.termed.util.service.SaveMode;
 import fi.thl.termed.util.service.Service;
-import fi.thl.termed.util.spring.exception.NotFoundException;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
@@ -37,25 +31,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class NodeCodeAdminController {
 
   @Autowired
-  private Service<TypeId, Type> typeService;
-
-  @Autowired
   private Service<NodeId, Node> nodeService;
 
   @DeleteMapping("/graphs/{graphId}/node-codes")
   @ResponseStatus(NO_CONTENT)
-  public void resetGraphNodeCodes(
+  public void deleteGraphNodeCodes(
       @PathVariable("graphId") UUID graphId,
       @RequestParam(name = "sync", defaultValue = "false") boolean sync,
       @AuthenticationPrincipal User user) {
     if (user.getAppRole() == AppRole.SUPERUSER) {
-      Map<TypeId, String> prefixes = typeService.getValues(new TypesByGraphId(graphId), user)
-          .stream()
-          .collect(Collectors.toMap(Type::identifier, Type::getNodeCodePrefixOrDefault));
-
       try (Stream<Node> stream = nodeService.getValueStream(new NodesByGraphId(graphId), user)) {
         List<Node> nodes = stream
-            .peek(node -> node.setCode(prefixes.get(node.getType()) + node.getNumber()))
+            .peek(node -> node.setCode(null))
             .collect(toList());
 
         nodeService.save(nodes, SaveMode.UPDATE, opts(sync), user);
@@ -67,21 +54,17 @@ public class NodeCodeAdminController {
 
   @DeleteMapping("/graphs/{graphId}/types/{typeId}/node-codes")
   @ResponseStatus(NO_CONTENT)
-  public void resetTypeNodeCodes(
+  public void deleteTypeNodeCodes(
       @PathVariable("graphId") UUID graphId,
       @PathVariable("typeId") String typeId,
       @RequestParam(name = "sync", defaultValue = "false") boolean sync,
       @AuthenticationPrincipal User user) {
     if (user.getAppRole() == AppRole.SUPERUSER) {
-      String prefix = typeService.get(TypeId.of(typeId, graphId), user)
-          .map(Type::getNodeCodePrefixOrDefault)
-          .orElseThrow(NotFoundException::new);
-
       try (Stream<Node> stream = nodeService.getValueStream(
           and(new NodesByGraphId(graphId),
               new NodesByTypeId(typeId)), user)) {
         List<Node> nodes = stream
-            .peek(node -> node.setCode(prefix + node.getNumber()))
+            .peek(node -> node.setCode(null))
             .collect(toList());
 
         nodeService.save(nodes, SaveMode.UPDATE, opts(sync), user);
