f19e7ddebdf734a29dd54102879d341195a9060c
Jussi Kurki <jussi.kurki@iki.fi>
2019-03-20 13:45:30 +0200

    refactor jena model http reading/writing, default rdf/xml to flat format

--- src/main/java/fi/thl/termed/util/jena/JenaModelMessageConverter.java
+++ src/main/java/fi/thl/termed/util/jena/JenaModelMessageConverter.java
@@ -1,5 +1,7 @@
 package fi.thl.termed.util.jena;
 
+import static org.apache.jena.riot.RDFFormat.RDFXML_PLAIN;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import fi.thl.termed.util.rdf.RdfMediaTypes;
@@ -7,6 +9,8 @@ import java.io.IOException;
 import java.util.Map;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
@@ -19,12 +23,12 @@ import org.springframework.http.converter.HttpMessageNotWritableException;
  */
 public class JenaModelMessageConverter extends AbstractHttpMessageConverter<Model> {
 
-  private Map<MediaType, String> mediaTypes = ImmutableMap.<MediaType, String>builder()
-      .put(RdfMediaTypes.N_TRIPLES, "N-TRIPLE")
-      .put(RdfMediaTypes.RDF_XML, "RDF/XML")
-      .put(RdfMediaTypes.LD_JSON, "JSON-LD")
-      .put(RdfMediaTypes.TURTLE, "TURTLE")
-      .put(RdfMediaTypes.N3, "N3")
+  private final Map<MediaType, RDFFormat> mediaTypes = ImmutableMap.<MediaType, RDFFormat>builder()
+      .put(RdfMediaTypes.N_TRIPLES, RDFFormat.NTRIPLES)
+      .put(RdfMediaTypes.RDF_XML, RDFFormat.RDFXML_PLAIN)
+      .put(RdfMediaTypes.LD_JSON, RDFFormat.JSONLD)
+      .put(RdfMediaTypes.TURTLE, RDFFormat.TURTLE)
+      .put(RdfMediaTypes.N3, RDFFormat.TURTLE)
       .build();
 
   public JenaModelMessageConverter() {
@@ -37,20 +41,19 @@ public class JenaModelMessageConverter extends AbstractHttpMessageConverter<Mode
   }
 
   @Override
-  protected Model readInternal(Class<? extends Model> cls, HttpInputMessage httpInputMessage)
+  protected Model readInternal(Class<? extends Model> cls, HttpInputMessage input)
       throws IOException, HttpMessageNotReadableException {
-
     Model model = ModelFactory.createDefaultModel();
-    return model.read(httpInputMessage.getBody(), null,
-        mediaTypes.get(httpInputMessage.getHeaders().getContentType()));
+    RDFFormat format = mediaTypes.getOrDefault(input.getHeaders().getContentType(), RDFXML_PLAIN);
+    RDFDataMgr.read(model, input.getBody(), null, format.getLang());
+    return model;
   }
 
   @Override
-  protected void writeInternal(Model model, HttpOutputMessage httpOutputMessage)
+  protected void writeInternal(Model model, HttpOutputMessage output)
       throws IOException, HttpMessageNotWritableException {
-
-    model.write(httpOutputMessage.getBody(),
-        mediaTypes.get(httpOutputMessage.getHeaders().getContentType()));
+    RDFFormat format = mediaTypes.getOrDefault(output.getHeaders().getContentType(), RDFXML_PLAIN);
+    RDFDataMgr.write(output.getBody(), model, format);
   }
 
 }
