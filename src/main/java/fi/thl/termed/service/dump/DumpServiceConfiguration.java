819fec7f2a0ffc01d2cc525512074ea74ced9ea8
Jussi Kurki <jussi.kurki@iki.fi>
2019-04-05 13:31:40 +0300

    remove buggy tx from dump; save graphs, types, nodes in own transactions

--- src/main/java/fi/thl/termed/service/dump/DumpServiceConfiguration.java
+++ src/main/java/fi/thl/termed/service/dump/DumpServiceConfiguration.java
@@ -3,7 +3,6 @@ package fi.thl.termed.service.dump;
 import static fi.thl.termed.domain.AppRole.ADMIN;
 import static fi.thl.termed.domain.AppRole.SUPERUSER;
 
-import com.google.common.eventbus.EventBus;
 import fi.thl.termed.domain.Dump;
 import fi.thl.termed.domain.DumpId;
 import fi.thl.termed.domain.Graph;
@@ -12,17 +11,13 @@ import fi.thl.termed.domain.Node;
 import fi.thl.termed.domain.NodeId;
 import fi.thl.termed.domain.Type;
 import fi.thl.termed.domain.TypeId;
-import fi.thl.termed.domain.event.InvalidateCachesEvent;
 import fi.thl.termed.util.service.ProfilingService;
 import fi.thl.termed.util.service.Service;
-import fi.thl.termed.util.service.TransactionalService;
-import fi.thl.termed.util.service.WriteErrorHandlingService;
 import fi.thl.termed.util.service.WriteLoggingService;
 import fi.thl.termed.util.service.WritePreAuthorizingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class DumpServiceConfiguration {
@@ -36,20 +31,11 @@ public class DumpServiceConfiguration {
   @Autowired
   private Service<NodeId, Node> nodeService;
 
-  @Autowired
-  private PlatformTransactionManager transactionManager;
-
-  @Autowired
-  private EventBus eventBus;
-
   @Bean
   public Service<DumpId, Dump> dumpService() {
     Service<DumpId, Dump> service =
         new DelegatingDumpService(graphService, typeService, nodeService);
 
-    service = new TransactionalService<>(service, transactionManager);
-    service = new WriteErrorHandlingService<>(service, (x) -> {
-    }, () -> eventBus.post(new InvalidateCachesEvent()));
     service = new WritePreAuthorizingService<>(service,
         (user) -> user.getAppRole() == SUPERUSER || user.getAppRole() == ADMIN,
         (user) -> false);
