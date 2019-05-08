d089c940d9b40176cd44c250ae861b2e6ae7a352
Jussi Kurki <jussi.kurki@iki.fi>
2019-05-08 16:12:56 +0300

    experiment node id remapping for graph/type

---
+++ src/main/java/fi/thl/termed/service/node/specification/NodesByNumberRange.java
@@ -0,0 +1,98 @@
+package fi.thl.termed.service.node.specification;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import fi.thl.termed.domain.Node;
+import fi.thl.termed.domain.NodeId;
+import fi.thl.termed.util.query.LuceneSpecification;
+import fi.thl.termed.util.query.ParametrizedSqlQuery;
+import fi.thl.termed.util.query.SqlSpecification;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.search.Query;
+
+public class NodesByNumberRange
+    implements LuceneSpecification<NodeId, Node>, SqlSpecification<NodeId, Node> {
+
+  private final Long lower;
+  private final Long upper;
+
+  public NodesByNumberRange(Long lower, Long upper) {
+    this.lower = lower;
+    this.upper = upper;
+  }
+
+  public static NodesByNumberRange of(Long lower, Long upper) {
+    return new NodesByNumberRange(lower, upper);
+  }
+
+  @Override
+  public boolean test(NodeId nodeId, Node node) {
+    Preconditions.checkArgument(Objects.equals(nodeId, new NodeId(node)));
+    Preconditions.checkNotNull(node.getNumber());
+
+    Long number = node.getNumber();
+
+    return (lower == null || number >= lower)
+        && (upper == null || number <= upper);
+  }
+
+  @Override
+  public Query luceneQuery() {
+    return LongPoint.newRangeQuery("number",
+        lower != null ? lower : Long.MIN_VALUE,
+        upper != null ? upper : Long.MAX_VALUE);
+  }
+
+  @Override
+  public ParametrizedSqlQuery sql() {
+    List<String> clauses = new ArrayList<>();
+    List<Object> params = new ArrayList<>();
+
+    if (lower != null) {
+      clauses.add("number >= ?");
+      params.add(lower);
+    }
+    if (upper != null) {
+      clauses.add("number <= ?");
+      params.add(upper);
+    }
+
+    if (clauses.isEmpty()) {
+      return ParametrizedSqlQuery.of("1 = 1");
+    }
+
+    return ParametrizedSqlQuery.of(
+        String.join(" AND ", clauses),
+        params.toArray(new Object[0]));
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NodesByNumberRange that = (NodesByNumberRange) o;
+    return Objects.equals(lower, that.lower) &&
+        Objects.equals(upper, that.upper);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(lower, upper);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("lower", lower)
+        .add("upper", upper)
+        .toString();
+  }
+
+}
---
+++ src/main/java/fi/thl/termed/web/admin/NodeIdAdminController.java
@@ -0,0 +1,159 @@
+package fi.thl.termed.web.admin;
+
+import static fi.thl.termed.util.collect.StreamUtils.findFirstAndClose;
+import static fi.thl.termed.util.collect.StreamUtils.toImmutableListAndClose;
+import static fi.thl.termed.util.query.AndSpecification.and;
+import static fi.thl.termed.util.query.Queries.query;
+import static fi.thl.termed.util.query.Sort.sort;
+import static fi.thl.termed.util.query.Sort.sortDesc;
+import static fi.thl.termed.util.service.WriteOptions.opts;
+import static java.util.stream.Collectors.toMap;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedGenerator;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.AbstractIterator;
+import com.google.common.collect.Multimaps;
+import fi.thl.termed.domain.AppRole;
+import fi.thl.termed.domain.Node;
+import fi.thl.termed.domain.NodeId;
+import fi.thl.termed.domain.User;
+import fi.thl.termed.service.node.specification.NodesByGraphId;
+import fi.thl.termed.service.node.specification.NodesByNumberRange;
+import fi.thl.termed.service.node.specification.NodesByTypeId;
+import fi.thl.termed.util.query.Specification;
+import fi.thl.termed.util.service.SaveMode;
+import fi.thl.termed.util.service.Service;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalLong;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class NodeIdAdminController {
+
+  @Autowired
+  private Service<NodeId, Node> nodeService;
+
+  private TimeBasedGenerator timeBasedGenerator = Generators.timeBasedGenerator();
+
+  @DeleteMapping(value = "/graphs/{graphId}/node-ids")
+  @ResponseStatus(NO_CONTENT)
+  public void regenerateGraphNodeIds(
+      @PathVariable("graphId") UUID graphId,
+      @RequestParam(name = "sync", defaultValue = "false") boolean sync,
+      @AuthenticationPrincipal User user) {
+    if (user.getAppRole() == AppRole.SUPERUSER) {
+      valuesInPages(NodesByGraphId.of(graphId), 10_000, user)
+          .forEachRemaining(nodes -> regenerateNodeIds(nodes, sync, user));
+    } else {
+      throw new AccessDeniedException("");
+    }
+  }
+
+  @DeleteMapping("/graphs/{graphId}/types/{typeId}/node-ids")
+  @ResponseStatus(NO_CONTENT)
+  public void regenerateTypeNodeIds(
+      @PathVariable("graphId") UUID graphId,
+      @PathVariable("typeId") String typeId,
+      @RequestParam(name = "sync", defaultValue = "false") boolean sync,
+      @AuthenticationPrincipal User user) {
+    if (user.getAppRole() == AppRole.SUPERUSER) {
+      valuesInPages(and(NodesByGraphId.of(graphId), NodesByTypeId.of(typeId)), 10_000, user)
+          .forEachRemaining(nodes -> regenerateNodeIds(nodes, sync, user));
+    } else {
+      throw new AccessDeniedException("");
+    }
+  }
+
+  private Iterator<List<Node>> valuesInPages(
+      Specification<NodeId, Node> specification, int pageSize, User user) {
+
+    Optional<Long> upperOptional = findFirstAndClose(
+        nodeService.values(query(specification, sortDesc("number"), 1), user))
+        .map(Node::getNumber);
+
+    if (!upperOptional.isPresent()) {
+      return Collections.emptyIterator();
+    }
+
+    long upperNumber = upperOptional.get();
+
+    return new AbstractIterator<List<Node>>() {
+      long lowerNumber = 0;
+
+      @Override
+      protected List<Node> computeNext() {
+        List<Node> nodes = toImmutableListAndClose(nodeService.values(query(
+            and(specification, NodesByNumberRange.of(lowerNumber, upperNumber)),
+            sort("number"),
+            pageSize), user));
+
+        OptionalLong lowestNumberOptional = nodes.stream()
+            .map(Node::getNumber).mapToLong(Long::longValue).max();
+
+        if (lowestNumberOptional.isPresent()) {
+          lowerNumber = lowestNumberOptional.getAsLong() + 1;
+          return nodes;
+        } else {
+          return endOfData();
+        }
+      }
+    };
+  }
+
+  private void regenerateNodeIds(List<Node> nodes, boolean sync, User user) {
+    LoadingCache<NodeId, Node> nodeCache = CacheBuilder.newBuilder().build(
+        CacheLoader.from(key -> nodeService.get(key, user).orElse(null)));
+
+    // pre-fill cache with ready available nodes
+    nodes.forEach(n -> nodeCache.put(n.identifier(), n));
+
+    Map<NodeId, NodeId> oldToNewIdMap = nodes.stream()
+        .collect(toMap(
+            Node::identifier,
+            node -> NodeId.of(timeBasedGenerator.generate(), node.getType())));
+
+    Stream<Node> remappedNodes = Stream.concat(
+        nodes.stream().map(Node::identifier),
+        nodes.stream().flatMap(node -> node.getReferrers().values().stream()))
+        .distinct()
+        .map(nodeCache::getUnchecked)
+        .map(node ->
+            Node.builder()
+                .id(oldToNewIdMap.getOrDefault(
+                    node.identifier(),
+                    node.identifier()))
+                .copyOptionalsFrom(node)
+                .references(
+                    Multimaps.transformValues(
+                        node.getReferences(),
+                        id -> oldToNewIdMap.getOrDefault(id, id)))
+                .build());
+
+    nodeService.saveAndDelete(
+        remappedNodes,
+        nodes.stream().map(Node::identifier),
+        SaveMode.UPSERT,
+        opts(sync),
+        user);
+  }
+
+}
