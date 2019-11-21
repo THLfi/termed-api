746786777a3a60b284dacbbca15dc9ef7f724106
Jussi Kurki <jussi.kurki@iki.fi>
2019-11-21 13:06:33 +0200

    document node revisions API

---
+++ src/test/java/fi/thl/termed/web/docs/NodeRevisionApiDocumentingIntegrationTest.java
@@ -0,0 +1,106 @@
+package fi.thl.termed.web.docs;
+
+import static fi.thl.termed.util.RegularExpressions.CODE;
+import static fi.thl.termed.web.docs.DocsExampleData.exampleGraphId;
+import static fi.thl.termed.web.docs.DocsExampleData.exampleNode0;
+import static fi.thl.termed.web.docs.DocsExampleData.exampleNode0Id;
+import static fi.thl.termed.web.docs.OperationIntroSnippet.operationIntro;
+import static io.restassured.RestAssured.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+class NodeRevisionApiDocumentingIntegrationTest extends BaseNodeApiDocumentingIntegrationTest {
+
+  @Test
+  void documentGetNodeRevisions() {
+    // do an extra update to populate revision history
+    given(adminAuthorizedJsonSaveRequest)
+        .body(exampleNode0)
+        .post("/api/graphs/{graphId}/nodes?mode=update", exampleGraphId.getId())
+        .then()
+        .statusCode(HttpStatus.SC_OK);
+
+    given(adminAuthorizedJsonGetRequest)
+        .filter(document("get-node-revisions",
+            operationIntro("Get an array of node revisions by node id. Objects in array contain "
+                + "only the \"metadata\". Complete revisions need to be loaded separately."),
+            pathParameters(
+                parameterWithName("graphId")
+                    .description("Graph identifier (UUID)"),
+                parameterWithName("typeId")
+                    .description("Type identifier (matches `" + CODE + "`)"),
+                parameterWithName("id")
+                    .description("Node identifier (UUID)")),
+            requestHeaders(
+                headerWithName("Authorization")
+                    .description("Basic authentication credentials")),
+            responseFields(
+                fieldWithPath("[].number")
+                    .description("Revision number, integer."),
+                fieldWithPath("[].author")
+                    .description("Author of the revision (username)."),
+                fieldWithPath("[].date")
+                    .description("Date of the revision in ISO format."),
+                subsectionWithPath("[].object")
+                    .description("Object revision, in this case just the Node identifier."))))
+        .when()
+        .get("/api/graphs/{graphId}/types/{typeId}/nodes/{id}/revisions",
+            exampleNode0Id.getTypeGraphId(),
+            exampleNode0Id.getTypeId(),
+            exampleNode0Id.getId())
+        .then()
+        .statusCode(HttpStatus.SC_OK);
+  }
+
+  @Test
+  void documentGetNodeRevision() {
+    given(adminAuthorizedJsonGetRequest)
+        .filter(document("get-a-node-revision",
+            operationIntro("Get a node revisions by node id and revision number."),
+            pathParameters(
+                parameterWithName("graphId")
+                    .description("Graph identifier (UUID)"),
+                parameterWithName("typeId")
+                    .description("Type identifier (matches `" + CODE + "`)"),
+                parameterWithName("id")
+                    .description("Node identifier (UUID)"),
+                parameterWithName("number")
+                    .description("Revision number (integer). If there is no revision with exactly "
+                        + "same revision number, finds the latest revision in respect to `number`. "
+                        + "I.e. a version that was valid at given revision. This functionality is "
+                        + "useful for traversing graph in past state.")),
+            requestHeaders(
+                headerWithName("Authorization")
+                    .description("Basic authentication credentials")),
+            responseFields(
+                fieldWithPath("number")
+                    .description("Revision number, integer."),
+                fieldWithPath("author")
+                    .description("Author of the revision (username)."),
+                fieldWithPath("date")
+                    .description("Date of the revision in ISO format."),
+                fieldWithPath("type")
+                    .description(
+                        "Revision type, possible values are `INSERT`, `UPDATE`, `DELETE`."),
+                subsectionWithPath("object")
+                    .description("Node as it was in given revision."))))
+        .when()
+        .get("/api/graphs/{graphId}/types/{typeId}/nodes/{id}/revisions/{number}",
+            exampleNode0Id.getTypeGraphId(),
+            exampleNode0Id.getTypeId(),
+            exampleNode0Id.getId(),
+            1)
+        .then()
+        .statusCode(HttpStatus.SC_OK);
+  }
+
+}
